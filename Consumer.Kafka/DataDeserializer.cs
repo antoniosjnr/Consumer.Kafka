949e3eee9332b19109674d5288673655f9e26c42
antonio.junior <antonio.sjnr@gmail.com>
2021-10-22 17:13:51 -0300

    Ajustes relacionados a deserialização de mensagens.

---
+++ Consumer.Kafka/DataDeserializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace Consumer.Kafka
+{
+    public class DataDeserializer<T> : IDeserializer<T>
+    {
+        public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+        {
+            var json = Encoding.UTF8.GetString(data);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
---
+++ Consumer.Kafka/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace Consumer.Kafka
+{
+    public class Order
+    {
+        public string Number { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
